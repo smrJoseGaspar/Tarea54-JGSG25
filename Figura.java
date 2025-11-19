dfe16855b6cd336dd6de01c495fd86c48665caae
José Gaspar Sánchez García <jg.sanchezgarcia@edu.gva.es>
2025-11-19 19:02:30 +0100

    Implementada interfaz Figura.java fix #1

---
+++ Figura.java
@@ -0,0 +1,12 @@
+package es.gva.edu.sanchezgarcia.jg.tarea.jgsg24;
+
+/**
+ *
+ * @author jgaspar
+ */
+public interface Figura {
+    public double calcularArea();
+    public double calcularPerimetro();
+    
+}
+
